39ee2826b014b8ac32866d77623b5365f4e8dc96
seonghyeon <hyeon@apartmentary.com>
2019-01-08 16:05:19 +0900

    add webpack image compression plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   module: {
@@ -18,6 +22,20 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/assets/images',
+      to: path.resolve(__dirname, "./dist/assets/images"),
+    }]),
+    new ImageminPlugin({
+      disable: process.env.NODE_ENV !== 'production',
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      pngquant: {
+        quality: '50'
+      },
+      plugins: [
+        imageminMozjpeg({ quality: '50' }),
+      ],
     }),
   ],
 };
