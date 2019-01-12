94ef9ff7e1f9afd88db13840a8e9f62bf5530e39
seonghyeon <hyeon@apartmentary.com>
2019-01-12 22:32:13 +0900

    add lazysizes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+import "lazysizes";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Img from "./assets/images/flower1.jpg";
+import Img2 from "./assets/images/bg-copy-4.png";
+
+const App = () => (
+  <div>
+    <h1>Hello React!</h1>
+    <img data-src={Img} className="lazyload" alt="flower" />
+    <div style={{ height: '1000px' }} />
+    <img data-src={Img2} className="lazyload" alt="flower" />
+  </div>
+);
+
 ReactDOM.render(
-  <div>Hello React!</div>,
+  <App />,
   document.getElementById("app"),
 );
