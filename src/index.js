7ceca3b156ee920c63c82c8de83c6f4ab58ecf38
seonghyeon <hyeon@apartmentary.com>
2019-01-14 18:59:01 +0900

    add basic mojs svg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import ReactDOM from "react-dom";
+import mojs from 'mo-js';
+
+
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    new mojs.Shape({
+      shape:        'circle',  // shape "circle" is default
+      radius:       50,
+      fill:         '#A8CABA',  //default is pink
+      stroke:       '#5D4157',
+      strokeWidth:  3,
+      isShowStart:  true,      // show before any animation starts
+    });
+  }
+
+  render() {
+    return (
+      <div>Hello Wolrd!</div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <div>Hello React!</div>,
+  <App />,
   document.getElementById("app"),
 );
