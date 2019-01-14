47216745691441ed83f6eda3b5619447e570bb51
seonghyeon <hyeon@apartmentary.com>
2019-01-14 18:13:54 +0900

    add basic animation using gsap TweenLite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import ReactDOM from "react-dom";
+import { TweenLite } from "gsap";
 
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.setRef = this.setRef.bind(this);
+  }
+
+  componentDidMount() {
+    this.tween1 = TweenLite.to(this.div1, 1, { x: 100 });
+    this.tween2 = TweenLite.to(this.div2, 1, { x: 100 });
+  }
+
+  setRef(name) {
+    return (element) => {
+      this[name] = element;
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ height: '100px', width: '100px', backgroundColor: 'red' }} ref={this.setRef('div1')} />
+        <div style={{ height: '100px', width: '100px', backgroundColor: 'red' }} ref={this.setRef('div2')} />
+      </div>
+    );
+  }
+}
 ReactDOM.render(
-  <div>Hello React!</div>,
+  <App />,
   document.getElementById("app"),
 );
